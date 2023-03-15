154e53db150b43a8df8cd6af90c74da7c0f0349f
carraes <carraeshb@gmail.com>
2023-03-15 11:10:01 -0300

    feat: main structure, middlewares, connect to db, first route to check if server is running

--- main.go
+++ main.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/carlosarraes/fsback/db"
+	"github.com/carlosarraes/fsback/handlers"
+	"github.com/carlosarraes/fsback/utils"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	port := 8080
+
+	db, err := db.Connect()
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+	defer db.Close()
+
+	app := &handlers.App{Db: db}
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(utils.Cors())
+
+	r.Get("/", app.CheckHealth)
+
+	log.Printf("Listening on port %d", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
+}
